6238a7c3f3a91ced5fbc88918c8384a11d74abe0
happycamper1129 <happycamper.sw@gmail.com>
2021-04-20 10:23:11 +0000

    Automatic Pin Updates

--- pkg/startup/startup.go
+++ pkg/startup/startup.go
@@ -939,7 +939,6 @@ func configureIPPools(ctx context.Context, client client.Interface, kubeadmConfi
 		ip, _, err := cnet.ParseCIDR(p.Spec.CIDR)
 		if err != nil {
 			log.Warnf("Error parsing CIDR '%s'. Skipping the IPPool.", p.Spec.CIDR)
-			continue
 		}
 		version := ip.Version()
 		ipv4Present = ipv4Present || (version == 4)
